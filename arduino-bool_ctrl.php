7f72051af69f9b40ab0b162dadca05a0be9db6a9
Syafiqmuda <syafiqmuda98@gmail.com>
2022-05-25 20:23:32 +0800

    New arduino controller

---
+++ arduino-bool_ctrl.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- CSS -->
+        <link href="css/modern.css" rel="stylesheet">
+    </head>
+    <body>
+		<div class="container-fluid">
+			<table class="table table-striped my-0">
+				<thead>
+					<tr>
+						<th>Controller</th>
+						<th class="text-start">Status</th>
+						<th class="text-start">Option</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Boolean 1</td>
+						<td class="text-start">Available</td>
+						<td class="text-start">
+							<button id="051" class="btn btn-outline-success">ON</button>
+							<button id="050" class="btn btn-outline-danger">OFF</button>
+						</td>
+					</tr>
+					<tr>
+						<td>Boolean 2</td>
+	
+						<td class="text-start">
+							Available
+						</td>
+						<td class="text-start">
+							<button id="061" class="btn btn-outline-success">ON</button>
+							<button id="060" class="btn btn-outline-danger">OFF</button>
+						</td>
+					</tr>
+					<tr>
+						<td>Boolean 3</td>
+						<td class="text-start">
+							Available
+						</td>
+						<td class="text-start">
+							<button id="071" class="btn btn-outline-success">ON</button>
+							<button id="070" class="btn btn-outline-danger">OFF</button>
+						</td>
+					</tr>
+					<tr>
+						<td>Boolean 4</td>
+							<td class="text-start">
+								Available
+							</td>
+						<td class="text-start">
+							<button id="081" class="btn btn-outline-success">ON</button>
+							<button id="080" class="btn btn-outline-danger">OFF</button>
+						</td>
+					</tr>
+					<tr>
+						<td>Boolean 5</td>
+							<td class="text-start">
+								Available
+							</td>
+						<td class="text-start">
+							<button id="091" class="btn btn-outline-success">ON</button>
+							<button id="090" class="btn btn-outline-danger" >OFF</button>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+    </body>
+    <script src="js/app.js"></script>
+</html>
