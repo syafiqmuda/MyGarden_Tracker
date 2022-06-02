283a78a71109130ae3c559e82bc3c20fa9cdba9c
Syafiqmuda <syafiqmuda98@gmail.com>
2022-06-02 11:18:45 +0800

    minor change

--- sql/config_online.php
+++ sql/config_online.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost'); //DB Host
-define('DB_USERNAME', 'id18175210_root');
-define('DB_PASSWORD', 'G@rd3nTr@ck3r'); 
-define('DB_NAME', 'id18175210_mygarden_tracker');
+define('DB_USERNAME', 'id19033293_localhost');
+define('DB_PASSWORD', '_<7[vG7oiPkk{T7c'); 
+define('DB_NAME', 'id19033293_mygarden_tracker');
  
 /* Attempt to connect to MySQL database */
 $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
